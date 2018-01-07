930d216148a9c40fe4c96afd60955a0f661d492f
elfoscur <elfoscur@gmail.com>
2018-01-07 11:52:32 +0100

    type function and if statement

---
+++ type_function.py
@@ -0,0 +1,21 @@
+
+a = 'hello '
+b = 'there'
+
+print(a+b)
+
+print(type(a)) #string - type return the type of the variable
+print(type(x)) #int
+print(type(1.2)) #float
+
+print(int(99.0)) #convert the float to int
+print(float(99)) #convert the int to float
+
+x = 5
+if x==5:
+    print('x equals to five')
+    print('x still equals to five')
+if x!=5:
+    print('x not equals to five')
+else:
+    print('x equals to '+str(x))
